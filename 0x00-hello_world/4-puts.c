9e0e671b06cff7d6d5c096dc5b9881dc3d20a5e2
FelixOmwenga <jjbiggy047@gmail.com>
2022-04-07 17:42:43 +0000

    bloody hell i'm just tired

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+
+puts(str);
+
+return (0);
+}
