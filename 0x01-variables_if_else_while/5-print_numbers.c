629c75b4eb41f54736666e1d90cf592db6d441db
FelixOmwenga <jjbiggy047@gmail.com>
2022-12-11 13:07:58 +0000

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+	while (x < 10)
+	{
+		printf("%d",x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
