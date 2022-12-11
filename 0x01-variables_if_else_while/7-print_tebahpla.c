fe23b5edad2c3293495f96186726962828036b61
FelixOmwenga <jjbiggy047@gmail.com>
2022-12-11 15:27:12 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - c print the alphabet in lowercase, reverse
+ *Return: 0
+ */
+int main(void)
+{
+	char x;
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
