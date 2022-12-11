78b2bf2d6b959e1d00c1914f2f0994b5b6b8f8a3
FelixOmwenga <jjbiggy047@gmail.com>
2022-12-11 16:15:20 +0000

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,21 +7,26 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 98; x++)
+	for (x = 0; x <= 99; x++)
 	{
-		for (y = x + 1; y < 99; y++)
+		for (y = 0; y < 99; y++)
 		{
-			putchar((x / 10) + '0');
-			putchar((x % 10) + '0');
-			putchar(' ');
-			putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
+			if (x < y && x != y)
+			{
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
+				putchar(' ');
+				putchar((y / 10) + '0');
+				putchar((y % 10) + '0');
 
-			if (x == 98 && x == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}	
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		
+		}
 	}
 	putchar('\n');
 	return (0);
