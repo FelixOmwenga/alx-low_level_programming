8e679d4ffb6053f205991ef00c0b849246a8fab9
FelixOmwenga <jjbiggy047@gmail.com>
2022-12-15 12:12:05 +0000

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main - print positive, zero or negative
- * Return: 0 
- */
+* main - print positive, zero or negative
+* Return: 0 
+*/
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
