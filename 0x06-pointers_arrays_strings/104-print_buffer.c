6b9ce9f009924fdd0089705d85c6bc427541258f
FelixOmwenga <jjbiggy047@gmail.com>
2023-01-09 19:52:53 +0000

    Backlog

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,43 +10,43 @@
  */
 void print_buffer(char *b, int size)
 {
-  int byte, index;
+	int byte, index;
 
-  for (byte = 0; byte < size; byte += 10)
-    {
-      printf("%08x: ", byte);
-
-      for (index = 0; index < 10; index++)
+	for (byte = 0; byte < size; byte += 10)
 	{
-	  if ((index + byte) >= size)
-	    printf("  ");
+		printf("%08x: ", byte);
 
-	  else
-	    printf("%02x", *(b + index + byte));
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf("  ");
 
-	  if ((index % 2) != 0 && index != 0)
-	    printf(" ");
-	}
+			else
+				printf("%02x", *(b + index + byte));
 
-      for (index = 0; index < 10; index++)
-	{
-	  if ((index + byte) >= size)
-	    break;
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
 
-	  else if (*(b + index + byte) >= 31 &&
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
 		   *(b + index + byte) <= 126)
-	    printf("%c", *(b + index + byte));
+				printf("%c", *(b + index + byte));
 
-	  else
-	    printf(".");
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
 	}
 
-      if (byte >= size)
-	continue;
-
-      printf("\n");
-    }
-
-  if (size <= 0)
-    printf("\n");
+	if (size <= 0)
+		printf("\n");
 }
