1a3fe3111ef0afe672ea9f7392b379f277dec48f
FelixOmwenga <jjbiggy047@gmail.com>
2023-01-09 20:24:19 +0000

    Backlog

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-  int i = 0;
-  int j;
+	int i = 0;
+	int j;
 
-  while (i < 8)
-    {
-      for (j = 0; j < 8; j++)
+	while (i < 8)
 	{
-	  _putchar(a[i][j]);
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	_putchar('\n');
+	i++;
 	}
-      _putchar('\n');
-      i++;
-    }
 }
