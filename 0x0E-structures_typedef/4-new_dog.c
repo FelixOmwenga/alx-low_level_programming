c9e878a6d3cd5f52fe7f1572ab4f88cddc1509b0
FelixOmwenga <jjbiggy047@gmail.com>
2023-01-17 19:56:55 +0000

    Small changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
+
 	p = malloc(sizeof(dog_t));
+
 	if (p == NULL)
 		return (NULL);
+
 	if (name == NULL)
 	{
 		free(p);
 		free(owner);
 		return (NULL);
 	}
+
 	if (owner == NULL)
 	{
 		free(p);
