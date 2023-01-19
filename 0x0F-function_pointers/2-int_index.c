3ac9371bda6812edf19994046609d1b88cd57c50
FelixOmwenga <jjbiggy047@gmail.com>
2023-01-19 19:04:35 +0000

    Day's work

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,10 +18,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (index = 0; index < size; index++)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (cmp(array[index]))
+			return (index);
 	}
-	return (-1)
+	return (-1);
 }
